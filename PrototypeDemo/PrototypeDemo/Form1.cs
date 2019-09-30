bd0a77b51270035679b2056d80c2245b678112a7
maob5579 <mantas.obolevicius@mif.stud.vu.lt>
2019-09-30 17:29:06 +0300

    Suradau mums reikiamo API demo appsa. Galima bus pritaikyti manau

--- PrototypeDemo/PrototypeDemo/Form1.cs
+++ PrototypeDemo/PrototypeDemo/Form1.cs
@@ -16,5 +16,10 @@ namespace PrototypeDemo
         {
             InitializeComponent();
         }
+
+        private void btnMeasure_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
