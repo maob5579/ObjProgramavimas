4e84482c6ccbbe50175dbabcc129daa0067878b8
margeviciutee <kamile.marge@gmail.com>
2019-10-13 03:14:05 +0300

    user/owner/admin forms

---
+++ PrototypeDemo/PrototypeDemo/FavRest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrototypeDemo
+{
+    public partial class FavRest : Form
+    {
+        public FavRest()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            UserForm newWindow = new UserForm();
+            newWindow.Closed += (s, args) => this.Close();
+            newWindow.Show();
+        }
+    }
+}
---
+++ PrototypeDemo/PrototypeDemo/UserForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrototypeDemo
+{
+    public partial class UserForm : Form
+    {
+        public UserForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 cameraWindow  = new Form1();
+            cameraWindow.Closed += (s, args) => this.Close();
+            cameraWindow.Show();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            RatedRest newWindow = new RatedRest();
+            newWindow.Closed += (s, args) => this.Close();
+            newWindow.Show();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 cameraWindow = new Form1();
+            cameraWindow.Closed += (s, args) => this.Close();
+            cameraWindow.Show();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 cameraWindow = new Form1();
+            cameraWindow.Closed += (s, args) => this.Close();
+            cameraWindow.Show();
+        }
+    }
+}
