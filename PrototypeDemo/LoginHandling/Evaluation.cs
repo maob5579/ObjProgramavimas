fc17b81938c2c8ffe2a4a5c11d30f680557e636a
ginterma <maciukonis1@gmail.com>
2019-10-22 20:25:30 +0300

    Patvarkytos biski admin formos , funkcianolumo įdėta. Parašyti metodai kurie pasiima visa data iš Restaurant lentelės duombazėj, sudeda juos y listą ir parodo juos consolėj.

---
+++ PrototypeDemo/LoginHandling/Evaluation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginHandling
+{
+    public class Evaluation
+    {
+        int evaluationId;
+        int userId;
+        int restaurantId;
+        int moodRating;
+        public Evaluation(int Evaluation, int User, int Restaurant, int Rating)
+        {
+            evaluationId = Evaluation;
+            userId = User;
+            restaurantId = Restaurant;
+            moodRating = Rating;
+        }
+        public int getEvaluationId()
+        {
+            return this.evaluationId;
+        }
+
+        public int getUserId()
+        {
+            return this.userId;
+        }
+        public int getRestaurantId()
+        {
+            return this.restaurantId;
+        }
+        
+        public int getMoodRating()
+        {
+            return this.moodRating;
+        }
+        
+        
+
+        public static void ShowEvaluations ()
+        {
+            List<Evaluation> evaluationList = DataBase.GetEvaluationList();
+            foreach(Evaluation element in evaluationList)
+            {
+                Console.WriteLine("{0} {1} {2} {3}", element.getEvaluationId(), element.getUserId(), element.getRestaurantId(), element.getMoodRating());
+           
+            }
+        }
+        
+    }
+}
--- PrototypeDemo/PrototypeDemo/OwnerForm.cs
+++ PrototypeDemo/PrototypeDemo/OwnerForm.cs
@@ -19,16 +19,26 @@ namespace PrototypeDemo
 
         private void btnAddRestaurant_Click(object sender, EventArgs e)
         {
-
+            this.Hide();
+            var regForm = new AddRestaurant();
+            regForm.Closed += (s, args) => this.Close();
+            regForm.Show();
         }
 
         private void btnDeleteRestaurant_Click(object sender, EventArgs e)
         {
-
+            this.Hide();
+            var regForm = new DeleteRestaurant();
+            regForm.Closed += (s, args) => this.Close();
+            regForm.Show();
         }
 
         private void btnViewRatings_Click(object sender, EventArgs e)
         {
+            this.Hide();
+            var regForm = new ViewRates();
+            regForm.Closed += (s, args) => this.Close();
+            regForm.Show();
 
         }
 
