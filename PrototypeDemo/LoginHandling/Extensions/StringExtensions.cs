b92031a698dbd745e23ce905ef2d514bd6429b1e
rimwme <tinginuxas09@gmail.com>
2019-10-22 23:34:51 +0300

    added regex and extension method

---
+++ PrototypeDemo/LoginHandling/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LoginHandling.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool ContainsUpperLowerNumber(this string str)
+        {
+            Regex rx = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            return rx.IsMatch(str);
+        }
+    }
+}
