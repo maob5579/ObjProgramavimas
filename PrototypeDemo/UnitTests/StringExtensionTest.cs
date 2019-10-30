7bb7f4a9b9da30ee5a275d363c7a932b5cccc1f6
rimwme <tinginuxas09@gmail.com>
2019-10-30 17:35:29 +0200

    Added 2 unit test methods for string extension

---
+++ PrototypeDemo/UnitTests/StringExtensionTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using LoginHandling.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class StringExtensionTest
+    {
+        [TestMethod]
+        public void ContainsUpperLowerNumber_DontContainNumber_False()
+        {
+            //Arrange
+            string str = "AsdasdAd";
+
+            //Act
+            var result = str.ContainsUpperLowerNumber();
+
+            //Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void ContainsUpperLowerNumber_ContainsEverything_True()
+        {
+            //Arrange
+            string str = "Aa12dA";
+
+            //Act
+            var result = str.ContainsUpperLowerNumber();
+
+            //Asert
+            Assert.IsTrue(result);
+        }
+    }
+}
